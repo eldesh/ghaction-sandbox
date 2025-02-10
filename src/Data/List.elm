169d095c6fe6688e8641f3412c6f218b8019c052
eldesh <nephits@gmail.com>
2025-02-10 22:42:45 +0900

    Add: some list operations and basic function from the core package

--- src/Data/List.elm
+++ src/Data/List.elm
@@ -1,6 +1,57 @@
-module Data.List exposing (List)
+module Data.List exposing
+    ( and
+    , map
+    , or
+    , scanl
+    , uncons
+    )
+
+import List exposing (map)
 
 
-type List a
-    = Nil
-    | Cons a (List a)
+map =
+    List.map
+
+
+uncons : List a -> Maybe ( a, List a )
+uncons list =
+    case list of
+        [] ->
+            Nothing
+
+        x :: xs ->
+            Just ( x, xs )
+
+
+and : List Bool -> Bool
+and list =
+    case list of
+        [] ->
+            True
+
+        x :: xs ->
+            x && and xs
+
+
+or : List Bool -> Bool
+or list =
+    case list of
+        [] ->
+            False
+
+        x :: xs ->
+            x || or xs
+
+
+
+-- Building Lists
+
+
+scanl : (b -> a -> b) -> b -> List a -> List b
+scanl f init list =
+    case list of
+        [] ->
+            [init]
+
+        x :: xs ->
+            init :: scanl f (f init x) xs
