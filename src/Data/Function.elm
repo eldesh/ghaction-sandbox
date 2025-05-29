9e647bf1386964a38e56029bcd4aa47b753cec2f
eldesh <nephits@gmail.com>
2025-05-29 22:36:13 +0900

    Add: Data.Function.fix

--- src/Data/Function.elm
+++ src/Data/Function.elm
@@ -1,11 +1,11 @@
-module Data.Function exposing (id, const, flip, on, applyWhen)
+module Data.Function exposing (id, const, flip, on, applyWhen, fix)
 
 {-| Small but handy combinators for building and transforming functions.
 
 These helpers make it easy to compose complex behaviour on the spot,
 so you can avoid writing ad‑hoc lambdas or intermediate definitions.
 
-@docs id, const, flip, on, applyWhen
+@docs id, const, flip, on, applyWhen, fix
 
 -}
 
@@ -29,6 +29,42 @@ const x _ =
 flip : (a -> b -> c) -> b -> a -> c
 flip f x y =
     f y x
+
+
+{-| The least fixed point of the function f, i.e. the least defined x
+such that f x = x.
+
+
+## Examples
+
+    fix
+        (\rec n ->
+            if n <= 1 then
+                1
+
+            else
+                n * rec (n - 1)
+        )
+        5
+        == 120
+
+    map
+        (fix
+            (\rec n ->
+                if n < 2 then
+                    n
+
+                else
+                    rec (n - 1) + rec (n - 2)
+            )
+        )
+        (range 1 10)
+        == [ 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ]
+
+-}
+fix : ((a -> b) -> (a -> b)) -> a -> b
+fix f x =
+    f (fix f) x
 
 
 {-| Apply a projection to two values and then combine the results with the given binary function.
